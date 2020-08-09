90ff2a6a840de4a9b5bc1f99204dee49583e694a
bsitenga <bsitenga@gmail.com>
2020-08-09 13:15:47 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Scrollspy from 'react-scrollspy';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="master-container">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Scrollspy items={['section-1', 'section-2', 'section-3']} currentClassName="is-current">
+        <li><a href="#section-1">section 1</a></li>
+        <li><a href="#section-2">section 2</a></li>
+        <li><a href="#section-3">section 3</a></li>
+      </Scrollspy>
+      <h1>jo, U'b Ntoam</h1>
     </div>
   );
 }
