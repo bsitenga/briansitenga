260cada4712162eeaa3623b73019637e67934f32
bsitenga <bsitenga@gmail.com>
2020-08-09 14:17:26 -0400

    initial scrollspy and navbar styling

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Nav from 'react-bootstrap/Nav';
 function App() {
   return (
     <div className="master-container">
-      <Scrollspy items={['welcome', 'about-me', 'portfolio']} currentClassName="is-current">
-        <Navbar bg="light" expand="lg">
+      {/* Navigation */}
+      <Scrollspy style={{ paddingLeft: '0', marginBottom: '0' }} className="scrollspy" items={['welcome', 'about-me', 'portfolio', 'skills', 'contact']} currentClassName="is-current">
+        <Navbar bg="light" expand="sm" fixed="top">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
+            <Nav className="mr-auto" id="#links-div">
               <Nav.Link href="#welcome">Welcome</Nav.Link>
               <Nav.Link href="#about-me">About Me</Nav.Link>
               <Nav.Link href="#portfolio">Portfolio</Nav.Link>
@@ -21,31 +22,32 @@ function App() {
           </Navbar.Collapse>
         </Navbar>
       </Scrollspy>
+
+      {/* Welcome Hero */}
       <div id="welcome">
         <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
-        <h1>jo, U'b Ntoam</h1>
       </div>
+
+      {/* About Section */}
       <div id="about-me">
         <h1>jo, U'b Ntoam</h1>
       </div>
+
+      {/* Portfolio Section */}
       <div id="portfolio">
         <h1>jo, U'b Ntoam</h1>
       </div>
+
+      {/* Skills Section */}
       <div id="skills">
         <h1>jo, U'b Ntoam</h1>
       </div>
+
+      {/* Contact Section */}
       <div id="contact">
         <h1>jo, U'b Ntoam</h1>
       </div>
-    </div>
+    </div >
   );
 }
 
